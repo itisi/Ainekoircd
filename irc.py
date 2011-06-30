3603ee76f4a391147846bd07643e303e75a66472
Aaron Opela <itisi52@gmail.com>
2011-06-30 16:20:26 -0700

    parts/quits/ping timeouts added

--- irc.py
+++ irc.py
@@ -2,12 +2,18 @@ import traceback
 import string
 from server import Channel
 allowedchars = string.letters + string.digits + "_"
-
+def decolon(field):
+    if len(field) and field[0] == ":":
+        return field[1:]
+    else:
+        return field
 def refresh(): #called when this module is reloaded. this should reload all modules it loads that are subject to change
     pass
 def handle(server, client, message):
     if "cmd_" + message[0].lower() in globals():
         try:
+            if not client.initialized and not message[0].lower() in ["nick", "user"]:
+                client.quit("Killed: Being evil")
             globals()["cmd_" + message[0].lower()](server, client, message)
         except:
             traceback.print_exc()
@@ -25,24 +31,36 @@ def cmd_nick(server, client, message):
             return 0
     client.nick = nick
     server.nicks[nick.lower()] = client
+    if client.user and not client.initialized:
+        init(client)
 def cmd_user(server, client, message):
     if not client.initialized:
-        client.initialized = True
-    user = message[1]
-    endparts = message[2].split(" ",2)
-    if len(endparts) != 3:
-        client.realname = ""
-    else:
-        if endparts[2][0] == ":":
-            client.realname = endparts[2][1:]
+        user = message[1]
+        endparts = message[2].split(" ",2)
+        if len(endparts) != 3:
+            client.realname = ""
         else:
-            client.realname = endparts[2]
-    for char in user:
-        if not char in allowedchars:
-            client.servsend("I PITY THE FOOL THAT TRIES TO USE A BAD USERNAME")
-            client.connection.close()
-            return 0
-    client.user = user
+            if endparts[2][0] == ":":
+                client.realname = endparts[2][1:]
+            else:
+                client.realname = endparts[2]
+        for char in user:
+            if not char in allowedchars:
+                client.servsend("I PITY THE FOOL THAT TRIES TO USE A BAD USERNAME")
+                client.connection.close()
+                return 0
+        client.user = user
+        if client.nick:
+            init(client)
+def cmd_part(server, client, message):
+    if message[1].lower() in server.channels:
+        channel = server.channels[message[1].lower()]
+        if channel in client.channels:
+            channel.raw(":%s PART %s :%s" % (client.hostmask(), channel.name, decolon(message[2])))
+            client.channels.remove(channel)
+            channel.remove(client)
+def init(client):
+    client.initialized = True
     client.servsend("001 %s :Welcome to the AINEKO IRC Network %s!%s@%s" % (client.nick, client.nick, client.user, client.host))
     client.servsend("422 %s :MOTD File is missing" % (client.nick))
 def cmd_join(server, client, message):
@@ -58,6 +76,7 @@ def cmd_join(server, client, message):
                 chan.append(client)
                 server.channels[channel.lower()] = chan
         if good:
+            client.channels.add(server.channels[channel.lower()])
             chan = server.channels[channel.lower()]
             nicks = []
             for nick in chan.nicks:
@@ -69,10 +88,14 @@ def cmd_privmsg(server, client, message):
     if message[1][0] == "#":
         if message[1].lower() in server.channels:
             channel = server.channels[message[1].lower()]
-            for nick in channel.nicks:
-                if nick != client:
-                    nick.send(":%s PRIVMSG %s :%s" % (client.hostmask(), channel.name, message[2][1:]))
+            channel.message(client, message[2][1:])
     else:
         if message[1].lower() in server.nicks:
             nick = server.nicks[message[1].lower()]
             nick.send(":%s PRIVMSG %s :%s" % (client.hostmask(), channel.name, message[2][1:]))
+def cmd_quit(server, client, message):
+    message = message[1]
+    if len(message) and message[0] == ":":
+        message = message[1:]
+    client.quit(message)
+
--- server.py
+++ server.py
@@ -10,18 +10,25 @@ class Channel():
         self.name = name
     def append(self, nick):
         self.nicks.add(nick)
-    def message(self, client, message):
+    def remove(self, nick):
+        self.nicks.discard(nick)
+    def message(self, client, message, excludeme=True):
         for nick in self.nicks:
-            if nick != client:
+            if nick != client or not excludeme:
                 mess = ":%s PRIVMSG %s :%s" % (client.hostmask(), self.name, message)
-                client.send(mess)
+                nick.send(mess)
+    def raw(self, message, client=False, excludeme=True):
+        for nick in self.nicks:
+            if nick != client or not excludeme:
+                nick.send(message)
 class Server:
     def __init__(self):
-        self.port = 6666
-        self.host = ""
+        self.port = 6667
+        self.host = "216.151.3.132"
         self.connections = []
         self.nicks = {}
         self.channels = {}
+        self.servername = "lol.lol.lol"
     def run(self):
         self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.allow_reuse_address = True
@@ -34,6 +41,15 @@ class Server:
             self.connections.append(client)
             thread.start_new_thread(client.run,())
             print 'Connected by', client.address
+    def pinger(self):
+        while 1:
+            curtime = time.time()
+            time.sleep(30)
+            for nick in self.connections:
+                if (curtime - nick.lastcontact) > 90:
+                    nick.quit("Ping Timeout")
+                else:
+                    nick.send("PING :%s" % (self.servername))
         
 class Client:
     def __init__(self, parent, conn, addr):
@@ -43,39 +59,74 @@ class Client:
         self.initialized = False
         self.lastcontact = time.time()
         self.connected = self.lastcontact
-    def servsend(self, message):
-        self.connection.send(":lol.lol.lol " + message + "\r\n")
-        print ":lol.lol.lol " + message
-    def hostmask(self):
+        self.channels = set()
+        self.nick = False
+        self.realname = False
+        self.user = False
+        self.hasquit = False
+    def servsend(self, message): #sends :servername <message>
+        self.send(":%s %s " % (self.server.servername, message))
+    def hostmask(self): #returns nick!user@host
         return "%s!%s@%s" % (self.nick, self.user, self.host)
+    def contactweb(self, exclude=[]): #returns all of the users that can see you in at least one channel (used for quits and nicks)
+        users = set()
+        for channel in self.channels:
+            for nick in channel.nicks:
+                if not nick in exclude:
+                    users.add(nick)
+        return users
+    def quit(self, message="Quitting"):
+        self.hasquit = True
+        for user in self.contactweb([self]):
+            user.send(":%s QUIT :%s" % (self.hostmask(), message))
+        for channel in self.channels:
+            channel.remove(self)
+        self.server.connections.remove(self)
+        if self.nick and self.nick.lower() in self.server.nicks: #The only time this won't be true is if the user pings out before sending a valid NICK command.
+            del(self.server.nicks[self.nick.lower()])
+        try:
+            self.connection.close()
+        except:
+            pass
     def send(self, message):
-        self.connection.send(message + "\r\n")
+        try:
+            self.connection.send(message + "\r\n")
+        except: #should have exception values eventually to differentiate connection reset by peer from other issues.
+            pass #quit the client
         print message        
     def run(self):
-        self.host = socket.gethostbyaddr(self.address[0])[0] #this isn't in init to avoid hostname lookup latency preventing new connections.
-        while 1:
-            for line in self.getlines():
-                self.handle(line)
+        try:
+            self.host = socket.gethostbyaddr(self.address[0])[0] #this isn't in init to avoid hostname lookup latency preventing new connections.
+            while 1:
+                for line in self.getlines():
+                    self.handle(line)
+        except:
+            traceback.print_exc()
     def getlines(self):
         message = ""
         runtime = 0
         while message.rfind("\n") == -1 or message.rfind("\n") != len(message) - 1:
-            receive = self.connection.recv(1024)
-            message += receive
-            if not runtime:
-                starttime = time.time()
-            elif time() - starttime > 10:
-                print "Client did something unexpected. Attempting to recover."
-                break
+            try:
+                receive = self.connection.recv(1024)
+            except: #this should be expanded on with proper except codes
+                if not self.hasquit:
+                    self.quit("Connection reset by peer")
+            else:
+                message += receive
+                if not runtime:
+                    starttime = time.time()
+                elif time() - starttime > 10:
+                    print "Client did something unexpected. Attempting to recover."
+                    break
         return message.splitlines()
     def handle(self,line):
         self.lastcontact = time.time()
         print line
-        parts = line.split(" ",3)
+        parts = line.split(" ",2)
         while len(parts) <= 3:
             parts.append("")
         if parts[0] == "PING":
-            self.servsend("PONG " + "lol.lol.lol :" + parts[1])
+            self.servsend("PONG " + "%s :%s" % (self.server.servername, parts[1]))
         elif parts[0] == "PRIVMSG" and parts[2] == ":.reload":
             try:
                 reload(irc)
